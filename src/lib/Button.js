b72a56b777e3636d44f79aa49305ef2e425831ed
unknown <lknsaheed@gmail.com>
2023-03-04 15:26:30 +0100

    Omo

---
+++ src/lib/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./styles/button.module.css";
+const Button = () => {
+  return <button className={classes.button}>Button</button>;
+};
+
+export default Button;
---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+import Button from "./Button";
+import { ReduxProvider } from "./redux";
+import "../../src/styles/globals.css";
+
+export { Button, ReduxProvider };
